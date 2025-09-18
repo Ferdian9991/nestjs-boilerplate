bfd2ee8816d37ae7a1868ace61617567db96c52b
ferdian9991 <ferdisatria15@gmail.com>
2025-09-18 10:10:40 +0700

    feat: implement period seeder to create default period entry if not exists

--- src/common/database/seeds/period.seeder.ts
+++ src/common/database/seeds/period.seeder.ts
@@ -1,3 +1,4 @@
+import { CreatePeriodDto } from '@/modules/academic/periods/dto/create-period.dto';
 import { PeriodEntity } from '@/modules/academic/periods/entities/period.entity';
 import { DataSource } from 'typeorm';
 import { Seeder, SeederFactoryManager } from 'typeorm-extension';
@@ -7,6 +8,24 @@ export default class PeriodSeeder implements Seeder {
     dataSource: DataSource,
     factoryManager: SeederFactoryManager,
   ): Promise<void> {
+    // Make default latest periode
+    const periodRepository = dataSource.getRepository(PeriodEntity);
+    const nowYear = new Date().getFullYear();
+
+    const data: CreatePeriodDto = {
+      code: `${nowYear}2`,
+      name: `Periode ${nowYear}2`,
+    };
+
+    let period = await periodRepository.findOneBy({ code: data.code });
+
+    // Insert only one record with this code.
+    if (!period) {
+      const periodCreate = periodRepository.create(data);
+      period = await periodRepository.save(periodCreate);
+    }
+
+    // Get user factory from factory manager.
     const periodFactory = factoryManager.get(PeriodEntity);
 
     try {
